b8372c17527e97583d4026939a6185e47559050c
mitch cail <mitch.cail@gmail.com>
2014-04-29 10:49:37 -0400

    other initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Launchify::Application.config.secret_key_base = 'cc586f65373a8fe1ed6aef27d072fc402b551b3251b0661315c41263a030a521df92d8d98a3e56b166c9872b0458f487e4460d83d34aca2ebd4a055188e8a5a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Launchify::Application.config.session_store :cookie_store, key: '_launchify_session'
