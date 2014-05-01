973ea2bae0deac6faafa7e7b2ed2195e5504d4e2
mitch cail <mitch.cail@gmail.com>
2014-05-01 09:34:45 -0400

    added fields form fields for editing existing posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Launchify::Application.routes.draw do
   root "posts#index"
   get "posts" => "posts#index"
   get "posts/:id" => "posts#show", as: "post"
+  get "posts/:id/edit" => "posts#edit", as: "edit_post"
 end
