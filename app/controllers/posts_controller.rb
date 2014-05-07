372246c9341da5766dc06184be3be958209da6ee
Mitch Cail <mitch.cail@gmail.com>
2014-05-07 12:54:03 -0400

    finished CRUD on the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,31 @@ class PostsController < ApplicationController
 	def edit
 		@post = Post.find(params[:id])
 	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to @post
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.create(post_params)
+		redirect_to posts_url
+	end
+
+	def destroy
+		Post.find(params[:id]).destroy
+		redirect_to posts_url
+	end
+
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :link, :description)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Launchify::Application.routes.draw do
   
   root "posts#index"
-  get "posts" => "posts#index"
-  get "posts/:id" => "posts#show", as: "post"
-  get "posts/:id/edit" => "posts#edit", as: "edit_post"
+  resources :posts
 end
