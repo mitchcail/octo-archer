28bfd4b1a472b7d508a97b34a4d13de153a18e08
Mitch Cail <mitch.cail@gmail.com>
2014-06-01 16:14:19 -0400

    added some new cool schtuff

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,6 @@ class Comment < ActiveRecord::Base
 
   RATING = [1,2,3,4,5]
 
-  validates :rating, inclusion: {in: RATING,
-  								 message: 'must be between 1 and 5' }
+  #validates :rating, inclusion: {in: RATING,
+  #								 message: 'must be between 1 and 5' }
 end
