a8e792a94b1c46d32fb743e50737caca2e2ea472
Mitch Cail <mitch.cail@gmail.com>
2014-07-09 16:47:13 -0400

    added more authorization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,10 @@ class SessionsController < ApplicationController
     if user = User.authenticate(params[:email], params[:password])
       # session
       session[:user_id] = user.id
+
       flash[:notice] = "welcome back, #{user.name}"
-      redirect_to user
+      redirect_to(session[:intended_url] || user)
+      session[:intended_url] = nil
     else
       flash.now[:alert] = "Invalid email/password combination!"
       render :new
