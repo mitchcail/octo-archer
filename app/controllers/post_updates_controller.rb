2f60be1160815391c5d126bc7276d9bb8db9d6a6
Mitch Cail <mitch.cail@gmail.com>
2014-08-08 13:57:46 -0400

    added more to updates

--- app/controllers/post_updates_controller.rb
+++ app/controllers/post_updates_controller.rb
@@ -5,4 +5,26 @@ class PostUpdatesController < ApplicationController
     @post = Post.find(params[:post_id])
     @post_updates = @post.post_updates.all
   end
+
+  def new
+    @post = Post.find(params[:post_id])
+    @post_updates = @post.post_updates.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @post_updates = @post.post_updates.new(update_params)
+    if @post_updates.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def update_params
+    params.require(:post_update).permit(:content)
+  end
 end
